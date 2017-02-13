7a98940aa2130727804e1d4fa3f86b99ddfc00a4
harsh4723 <harsh4723@rediffmail.com>
2017-02-14 00:17:47 +0530

    Update directoryread.php

--- directoryread.php
+++ directoryread.php
@@ -1,12 +1,12 @@
 
 <?php
-$dir = 'G:\sometext\\';//directory name is "sometext".
+$dir = 'G:\sometext\\';     //directory name is "sometext".
 
 if (is_dir($dir)){
   if ($dh = opendir($dir)){    // to open a directory
     while ($file = readdir($dh)){   //to read from the directory
 		if($file!='.'&&$file!='..'){     //removing the deafault files in directory
-      echo trim($file)." contains <br>";
+      echo $file." contains <br>";
 	  $newloc=$dir."".$file ; 
 	   $readin=file($newloc);     //read data inside the file
 	  foreach($readin as $tel){
